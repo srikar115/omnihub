67e70277bd1205d07deadeefd5384d7275ffd85d
Bhadra <bhadra@eflairwebtech.com>
2026-01-20 15:47:55 +0530

    nestJS framework added

--- frontend/src/components/creator/index.js
+++
@@ -1,5 +0,0 @@
-export { CreatorBar } from './CreatorBar';
-export { ModelSelectorModal } from './ModelSelectorModal';
-export { ImageSourceModal } from './ImageSourceModal';
-export { MultiModelSettingsModal } from './MultiModelSettingsModal';
-export { UpscaleModal } from './UpscaleModal';
